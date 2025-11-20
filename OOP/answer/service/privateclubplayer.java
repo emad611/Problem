21bb88b015c31afe107faa691d6ddd548b1917b6
Emad Magdey <emadmagdey2@gmail.com>
2025-11-20 19:05:08 +0200

    problem

---
+++ OOP/answer/service/privateclubplayer.java
@@ -0,0 +1,5 @@
+package com.model.service;
+//عملنا اول interface علشان نعملها وراثة من كلاس المشابه
+public interface PrivateClubPlayer {
+    Player addPlayer(Player player);//هنا اضفنا header or prototype الى من خلاله هنعمل override لكلاس الابن
+}
---
+++ OOP/answer/service/publicclubplayerclassimpl.java
@@ -0,0 +1,8 @@
+package com.model.service;
+
+public class PublicClubClassImpl implements PublicClubPlayer {
+    @Override
+    public Player addPlayer(Player player) {
+        return player;
+    }
+}
