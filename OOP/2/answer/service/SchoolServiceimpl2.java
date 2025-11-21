51e7aca690f5247842dd85e0809ff13dbfaefaae
Emad Magdey <emadmagdey2@gmail.com>
2025-11-21 08:52:06 +0200

    probem

--- OOP/2/answer/service/SchoolServiceimpl2.java
+++ OOP/2/answer/service/SchoolServiceimpl2.java
@@ -4,6 +4,7 @@ import com.model.service.SchoolServiceImpl;
 import com.model.service.Student;
 
 public class SchoolServiceImpl2 extends SchoolServiceImpl {
+//هنا عملنا ميثود واحده بس override لانها imp method ف عادى وفى نفس الوقت طبقنا الشروط
     @Override
     public  void ShowStudent(Student student){
         System.out.println("Inside ShowStudent");
